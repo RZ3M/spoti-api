b020404f19218f4d8e4b23baae27cd138bc303c3
Jack <omnomnomny@gmail.com>
2024-11-22 21:47:02 -0500

    removed more deprecated

--- user-service.js
+++ user-service.js
@@ -18,9 +18,7 @@ let User;
 
 module.exports.connect = function () {
   return new Promise(function (resolve, reject) {
-    let db = mongoose.createConnection(mongoDBConnectionString, {
-      useNewUrlParser: true,
-    });
+    let db = mongoose.createConnection(mongoDBConnectionString);
     db.on("error", (err) => {
       reject(err);
     });
